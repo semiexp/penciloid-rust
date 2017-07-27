599f6feddf5ddafa0f0c5cb528be10128f4eff56
semiexp <linthya.v@gmail.com>
2017-07-27 22:43:21 +0900

    Kakuro: add answer_to_problem

--- src/kakuro/mod.rs
+++ src/kakuro/mod.rs
@@ -17,3 +17,42 @@ pub enum Clue {
 pub use self::field_shape::*;
 pub use self::dictionary::*;
 pub use self::field::*;
+
+use super::{Grid, Coord};
+pub fn answer_to_problem(ans: &Grid<i32>) -> Grid<Clue> {
+    let mut has_clue = Grid::new(ans.height(), ans.width(), false);
+    for y in 0..ans.height() {
+        for x in 0..ans.width() {
+            let loc = Coord { y: y, x: x };
+            has_clue[loc] = !(1 <= ans[loc] && ans[loc] <= MAX_VAL);
+        }
+    }
+    let shape = FieldShape::new(&has_clue);
+    let mut prob_base = Grid::new(ans.height(), ans.width(), (0, 0));
+    for y in 0..ans.height() {
+        for x in 0..ans.width() {
+            let val = ans[Coord { y: y, x: x }];
+            if !(1 <= val && val <= MAX_VAL) { continue; }
+
+            let (g1, g2) = shape.cell_to_groups[Coord { y: y, x: x }];
+            match shape.clue_locations[g1 as usize] {
+                ClueLocation::Horizontal(h) => prob_base[h as usize].0 += val,
+                ClueLocation::Vertical(v) => prob_base[v as usize].1 += val,
+            }
+            match shape.clue_locations[g2 as usize] {
+                ClueLocation::Horizontal(h) => prob_base[h as usize].0 += val,
+                ClueLocation::Vertical(v) => prob_base[v as usize].1 += val,
+            }
+        }
+    }
+    let mut ret = Grid::new(ans.height(), ans.width(), Clue::NoClue);
+    for y in 0..ans.height() {
+        for x in 0..ans.width() {
+            let loc = Coord { y: y, x: x };
+            let v = ans[loc];
+            if (1 <= v && v <= MAX_VAL) { continue; }
+            ret[loc] = Clue::Clue { horizontal: prob_base[loc].0, vertical: prob_base[loc].1 };
+        }
+    }
+    ret
+}
