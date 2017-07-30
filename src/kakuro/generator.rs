8c2c247dea946932c8cab71857e9b4312dd26258
semiexp <linthya.v@gmail.com>
2017-07-30 19:17:18 +0900

    Kakuro: enhance generator

--- src/kakuro/generator.rs
+++ src/kakuro/generator.rs
@@ -22,9 +22,9 @@ pub fn generate<R: Rng>(has_clue: &Grid<bool>, dic: &Dictionary, rng: &mut R) ->
     let mut temperature = 10.0f64;
 
     for step in 0..n_step {
-        let mut move_cand: Vec<Vec<(i32, i32, i32, i32)>> = vec![];
+        let mut move_cand: Vec<Vec<(usize, i32, i32)>> = vec![];
 
-        let mut grp_has_val = vec![0; field_shape.group_to_cells.len()];
+        let mut grp_val_loc = vec![[-1; (MAX_VAL + 1) as usize]; field_shape.group_to_cells.len()];
         for y in 0..height {
             for x in 0..width {
                 let loc = Coord { y: y, x: x };
@@ -32,28 +32,51 @@ pub fn generate<R: Rng>(has_clue: &Grid<bool>, dic: &Dictionary, rng: &mut R) ->
                     continue;
                 }
                 let (g1, g2) = field_shape.cell_to_groups[loc];
-                grp_has_val[g1 as usize] |= 1 << answer[loc];
-                grp_has_val[g2 as usize] |= 1 << answer[loc];
+                grp_val_loc[g1 as usize][answer[loc] as usize] = has_clue.index(loc) as i32;
+                grp_val_loc[g2 as usize][answer[loc] as usize] = has_clue.index(loc) as i32;
             }
         }
         for y in 0..height {
             for x in 0..width {
                 let loc = Coord { y: y, x: x };
+                let c = has_clue.index(loc);
                 if has_clue[loc] {
                     continue;
                 }
                 let (g1, g2) = field_shape.cell_to_groups[loc];
-                for c in 1..MAX_VAL {
-                    if answer[loc] == c {
+                for v in 1..(MAX_VAL + 1) {
+                    if answer[loc] == v {
                         continue;
                     }
-                    if (grp_has_val[g1 as usize] & (1 << c)) != 0 {
-                        continue;
+                    match (grp_val_loc[g1 as usize][v as usize], grp_val_loc[g2 as usize][v as usize]) {
+                        (-1, -1) => move_cand.push(vec![(c, answer[loc], v)]),
+                        (c1, -1) => {
+                            if answer[loc] < v && grp_val_loc[field_shape.cell_to_groups[c1 as usize].1 as usize][answer[loc] as usize] == -1 {
+                                move_cand.push(vec![
+                                    (c, answer[loc], v),
+                                    (c1 as usize, v, answer[loc]),
+                                ]);
+                            }
+                        },
+                        (-1, c2) => {
+                            if answer[loc] < v && grp_val_loc[field_shape.cell_to_groups[c2 as usize].0 as usize][answer[loc] as usize] == -1 {
+                                move_cand.push(vec![
+                                    (c, answer[loc], v),
+                                    (c2 as usize, v, answer[loc]),
+                                ]);
+                            }
+                        },
+                        (c1, c2) => {
+                            if grp_val_loc[field_shape.cell_to_groups[c1 as usize].1 as usize][answer[loc] as usize] == -1 &&
+                               grp_val_loc[field_shape.cell_to_groups[c2 as usize].0 as usize][answer[loc] as usize] == -1 {
+                                move_cand.push(vec![
+                                    (c, answer[loc], v),
+                                    (c1 as usize, v, answer[loc]),
+                                    (c2 as usize, v, answer[loc]),
+                                ]);
+                            }
+                        },
                     }
-                    if (grp_has_val[g2 as usize] & (1 << c)) != 0 {
-                        continue;
-                    }
-                    move_cand.push(vec![(y, x, answer[loc], c)]);
                 }
             }
         }
@@ -61,8 +84,8 @@ pub fn generate<R: Rng>(has_clue: &Grid<bool>, dic: &Dictionary, rng: &mut R) ->
         rng.shuffle(&mut move_cand);
 
         for cand in &move_cand {
-            for &(y, x, _, c) in cand {
-                answer[Coord { y: y, x: x }] = c;
+            for &(id, _, c) in cand {
+                answer[id] = c;
             }
             let problem = answer_to_problem(&answer);
             let mut field = Field::new(&problem, dic);
@@ -80,8 +103,8 @@ pub fn generate<R: Rng>(has_clue: &Grid<bool>, dic: &Dictionary, rng: &mut R) ->
                 current_total_cands = total_cands;
                 break;
             } else {
-                for &(y, x, old, _) in cand {
-                    answer[Coord { y: y, x: x }] = old;
+                for &(id, old, _) in cand {
+                    answer[id] = old;
                 }
             }
         }
