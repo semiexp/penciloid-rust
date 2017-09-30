f905893d8aae7a5ee28f888a54983298371597f7
semiexp <linthya.v@gmail.com>
2017-10-01 01:42:00 +0900

    Slitherlink: CRLF -> LF, check whether all clues are placed, consider only the clue positions near an undecided edge

--- src/slitherlink/generator.rs
+++ src/slitherlink/generator.rs
@@ -1,5 +1,5 @@
 use super::super::{Grid, Y, X, Coord};
-use grid_loop::GridLoopField;
+use grid_loop::{Edge, GridLoop, GridLoopField};
 use super::*;
 
 use rand::{Rng, distributions};
@@ -13,12 +13,24 @@ pub fn generate<R: Rng>(has_clue: &Grid<bool>, dic: &Dictionary, rng: &mut R) ->
     let mut prev_decided_edges = 0;
     let temperature = 5.0f64;
 
+    let mut unplaced_clues = 0;
+    for y in 0..height {
+        for x in 0..width {
+            if has_clue[(Y(y), X(x))] {
+                unplaced_clues += 1;
+            }
+        }
+    }
+
+    let mut last_field = Field::new(&current_problem, dic);
+
     for _ in 0..max_step {
         let mut pos_cand = vec![];
         for y in 0..height {
             for x in 0..width {
-                if has_clue[(Y(y), X(x))] {
-                    pos_cand.push((Y(y), X(x)));
+                let cd = (Y(y), X(x));
+                if has_clue[cd] && (current_problem[cd] == NO_CLUE || has_undecided_nearby(&last_field, cd)) {
+                    pos_cand.push(cd);
                 }
             }
         }
@@ -64,10 +76,15 @@ pub fn generate<R: Rng>(has_clue: &Grid<bool>, dic: &Dictionary, rng: &mut R) ->
                 if isok {
                     updated = true;
                     prev_decided_edges = current_decided_edges;
+                    if prev_clue == NO_CLUE {
+                        unplaced_clues -= 1;
+                    }
 
-                    if field.fully_solved() {
+                    if field.fully_solved() && unplaced_clues == 0 {
                         return Some(current_problem);
                     }
+
+                    last_field = field;
                     break;
                 } else {
                     current_problem[pos] = prev_clue;
@@ -81,6 +98,29 @@ pub fn generate<R: Rng>(has_clue: &Grid<bool>, dic: &Dictionary, rng: &mut R) ->
     }
 
     None
+}
+
+fn has_undecided_nearby(field: &Field, clue_pos: Coord) -> bool {
+    let (Y(y), X(x)) = clue_pos;
+    let y = y * 2 + 1;
+    let x = x * 2 + 1;
+
+    let neighbor_size: i32 = 7;
+    for dy in -neighbor_size..(neighbor_size + 1) {
+        let dx_max = neighbor_size - dy.abs();
+        for dx in -dx_max..(dx_max + 1) {
+            let y2 = y + dy;
+            let x2 = x + dx;
+            
+            let cd = (Y(y2), X(x2));
+            if (dy & 1) != (dx & 1) {
+                if field.get_edge_safe(cd) == Edge::Undecided {
+                    return true;
+                }
+            }
+        }
+    }
+    false
 }
 
 fn has_zero_nearby(problem: &Grid<Clue>, (Y(y), X(x)): Coord) -> bool {
