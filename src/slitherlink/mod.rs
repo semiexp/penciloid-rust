6c97a4c439b3f7e90625274f558e5147261147ab
semiexp <linthya.v@gmail.com>
2017-10-09 22:50:20 +0900

    Slitherlink: add format

--- src/slitherlink/mod.rs
+++ src/slitherlink/mod.rs
@@ -1,10 +1,12 @@
 mod field;
 mod dictionary;
 mod generator;
+mod format;
 
 pub use self::field::*;
 pub use self::dictionary::*;
 pub use self::generator::*;
+pub use self::format::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Clue(pub i32);
