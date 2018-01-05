e20c9857a76f0ba21e25f2352c0eb273059681a9
semiexp <linthya.v@gmail.com>
2018-01-05 22:59:12 +0900

    Numberlink: add PlacementGenerator to avoid a lot of memory allocations

--- src/numberlink/generator.rs
+++ src/numberlink/generator.rs
@@ -34,6 +34,7 @@ struct AnswerField {
     field: Grid<Edge>, // (2 * height - 1) * (2 * width - 1)
     seed_idx: Grid<i32>,
     seeds: Vec<Coord>,
+    seed_count: usize,
     endpoint_constraint: Grid<Endpoint>,
     endpoints: i32,
     chain_threshold: i32,
@@ -50,7 +51,8 @@ impl AnswerField {
             chain_length: Grid::new(height, width, 0),
             field: Grid::new(2 * height - 1, 2 * width - 1, Edge::Undecided),
             seed_idx: Grid::new(2 * height - 1, 2 * width - 1, -1),
-            seeds: vec![],
+            seeds: vec![(Y(0), X(0)); (height * width) as usize],
+            seed_count: 0,
             endpoint_constraint: match opt.endpoint_constraint {
                 Some(ep) => ep.clone(),
                 None => Grid::new(height, width, Endpoint::Any),
@@ -65,10 +67,11 @@ impl AnswerField {
             ret.chain_union[idx] = idx;
         }
 
-        ret.seeds.push((Y(0), X(0)));
-        ret.seeds.push((Y(0), X(2 * width - 2)));
-        ret.seeds.push((Y(2 * height - 2), X(0)));
-        ret.seeds.push((Y(2 * height - 2), X(2 * width - 2)));
+        ret.seeds[0] = (Y(0), X(0));
+        ret.seeds[1] = (Y(0), X(2 * width - 2));
+        ret.seeds[2] = (Y(2 * height - 2), X(0));
+        ret.seeds[3] = (Y(2 * height - 2), X(2 * width - 2));
+        ret.seed_count = 4;
         ret.seed_idx[(Y(0), X(0))] = 0;
         ret.seed_idx[(Y(0), X(2 * width - 2))] = 1;
         ret.seed_idx[(Y(2 * height - 2), X(0))] = 2;
@@ -126,15 +129,33 @@ impl AnswerField {
         let nb = self.count_neighbor(cd);
         nb == (0, 2) || (nb.0 == 1 && nb.1 > 0)
     }
-    fn num_seeds(&self) -> usize { self.seeds.len() }
+    fn num_seeds(&self) -> usize { self.seed_count }
+
+    /// Copy `src` into this `AnswerField`.
+    /// the shape of these `AnswerField`s must match.
+    fn copy_from(&mut self, src: &AnswerField) {
+        self.chain_union.copy_from(&src.chain_union);
+        self.chain_length.copy_from(&src.chain_length);
+        self.field.copy_from(&src.field);
+        self.seed_idx.copy_from(&src.seed_idx);
+
+        self.seeds[0..src.seed_count].copy_from_slice(&src.seeds[0..src.seed_count]);
+        self.seed_count = src.seed_count;
+
+        self.endpoint_constraint.copy_from(&src.endpoint_constraint);
+        self.endpoints = src.endpoints;
+        self.chain_threshold = src.chain_threshold;
+        self.forbid_adjacent_clue = src.forbid_adjacent_clue;
+        self.invalid = src.invalid;
+    }
 
     /// Returns whether there is at least one seed
     fn has_seed(&self) -> bool {
-        self.seeds.len() != 0
+        self.seed_count != 0
     }
     /// Returns a random seed using `rng`
     fn random_seed<R: Rng>(&self, rng: &mut R) -> Coord {
-        let idx = rng.gen_range(0, self.seeds.len());
+        let idx = rng.gen_range(0, self.seed_count);
         self.seeds[idx]
     }
     fn decide(&mut self, cd: Coord, state: Edge) {
@@ -341,14 +362,16 @@ impl AnswerField {
 
         if seed_idx != -1 && !is_seed {
             // (y, x) is no longer a seed
-            let moved = self.seeds[self.seeds.len() - 1];
+            let moved = self.seeds[self.seed_count - 1];
             self.seed_idx[moved] = seed_idx;
-            self.seeds.swap_remove(seed_idx as usize);
+            self.seeds[seed_idx as usize] = moved;
+            self.seed_count -= 1;
             self.seed_idx[(Y(y), X(x))] = -1;
         } else if seed_idx == -1 && is_seed {
             // (y, x) is now a seed
-            self.seed_idx[(Y(y), X(x))] = self.seeds.len() as i32;
-            self.seeds.push((Y(y), X(x)));
+            self.seed_idx[(Y(y), X(x))] = self.seed_count as i32;
+            self.seeds[self.seed_count] = (Y(y), X(x));
+            self.seed_count += 1;
         }
     }
     /// Extend the chain one of whose endpoint is `(y, x)`
@@ -372,6 +395,211 @@ impl AnswerField {
             (1, 0) => self.decide(end1_undecided[0], Edge::Line),
             _ => (),
         }
+    }
+}
+
+pub struct PlacementGenerator {
+    pool: Vec<AnswerField>,
+    active_fields: Vec<AnswerField>,
+    next_fields: Vec<AnswerField>,
+    height: i32,
+    width: i32,
+    beam_width: usize,
+}
+
+impl PlacementGenerator {
+    pub fn new(height: i32, width: i32) -> PlacementGenerator {
+        let template = AnswerField::new(height, width, &GeneratorOption {
+            chain_threshold: 1,
+            endpoint_constraint: None,
+            forbid_adjacent_clue: false,
+        });
+        let beam_width = 100;
+        PlacementGenerator {
+            pool: vec![template; beam_width * 2 + 1],
+            active_fields: Vec::with_capacity(beam_width),
+            next_fields: Vec::with_capacity(beam_width),
+            height,
+            width,
+            beam_width,
+        }
+    }
+    pub fn generate<R: Rng>(&mut self, opt: &GeneratorOption, rng: &mut R) -> Option<Grid<Clue>> {
+        let beam_width = self.beam_width;
+        let height = self.height;
+        let width = self.width;
+        let fields = &mut self.active_fields;
+
+        let template = AnswerField::new(height, width, opt);
+
+        let mut field_base = self.pool.pop().unwrap();
+        field_base.copy_from(&template);
+
+        fields.push(field_base);
+
+        loop {
+            if fields.len() == 0 { break; }
+
+            let fields_next = &mut self.next_fields;
+            'outer: for _ in 0..(5 * fields.len()) {
+                if fields_next.len() >= beam_width { break; }
+
+                let id = rng.gen_range(0, fields.len());
+                let mut field = self.pool.pop().unwrap();
+                field.copy_from(&fields[id]);
+
+                if !field.has_seed() { continue; }
+                let cd = field.random_seed(rng);
+                let (Y(y), X(x)) = cd;
+                let cd_vtx = (Y(y / 2), X(x / 2));
+
+                if field.count_neighbor(cd) == (0, 2) {
+                    let nbs = field.undecided_neighbors(cd);
+                    let constraint = field.endpoint_constraint(cd_vtx);
+                    if (constraint != Endpoint::Forced && rng.next_f64() < 0.9f64) || constraint == Endpoint::Prohibited {
+                        // as angle
+                        field.decide(nbs[0], Edge::Line);
+                        field.decide(nbs[1], Edge::Line);
+                    } else {
+                        // as an endpoint
+                        let i = rng.gen_range(0, 2);
+                        field.decide(nbs[i], Edge::Line);
+                        field.decide(nbs[(1 - i)], Edge::Blank);
+                    }
+                } else {
+                    let nbs = field.undecided_neighbors(cd);
+
+                    if rng.next_f64() < 1.0f64 {
+                        // extend
+                        let i = rng.gen_range(0, nbs.len());
+                        field.decide(nbs[i], Edge::Line);
+                    } else {
+                        // terminate
+                        for nb in nbs {
+                            field.decide(nb, Edge::Blank);
+                        }
+                    }
+                }
+
+                if field.invalid {
+                    // release this field
+                    self.pool.push(field);
+                    continue;
+                }
+                if field.num_seeds() == 0 {
+                    let mut ids = Grid::new(height, width, -1);
+                    let mut id = 1;
+                    for y in 0..height {
+                        for x in 0..width {
+                            if ids[(Y(y), X(x))] == -1 {
+                                fill_line_id((Y(y), X(x)), &field, &mut ids, id);
+                                id += 1;
+                            }
+                        }
+                    }
+
+                    let mut line_len = vec![0; id as usize];
+                    for y in 0..height {
+                        for x in 0..width {
+                            line_len[ids[(Y(y), X(x))] as usize] += 1;
+                        }
+                    }
+                    for i in 1..id {
+                        if line_len[i as usize] <= 3 { self.pool.push(field); continue 'outer; }
+                    }
+
+                    let mut end_count = vec![0; id as usize];
+                    for y in 0..height {
+                        for x in 0..width {
+                            if field.count_neighbor((Y(y * 2), X(x * 2))) == (1, 0) {
+                                end_count[ids[(Y(y), X(x))] as usize] += 1;
+                            }
+                        }
+                    }
+                    for i in 1..id {
+                        if end_count[i as usize] != 2 { self.pool.push(field); continue 'outer; }
+                    }
+
+                    for y in 0..(2 * height - 1) {
+                        for x in 0..(2 * width - 1) {
+                            if y % 2 == 1 && x % 2 == 0 {
+                                if (ids[(Y(y / 2), X(x / 2))] == ids[(Y(y / 2 + 1), X(x / 2))]) != (field.get((Y(y), X(x))) == Edge::Line) { self.pool.push(field); continue 'outer; }
+                            } else if y % 2 == 0 && x % 2 == 1 {
+                                if (ids[(Y(y / 2), X(x / 2))] == ids[(Y(y / 2), X(x / 2 + 1))]) != (field.get((Y(y), X(x))) == Edge::Line) { self.pool.push(field); continue 'outer; }
+                            }
+                        }
+                    }
+
+                    let mut ret = Grid::new(height, width, NO_CLUE);
+                    for y in 0..height {
+                        for x in 0..width {
+                            if field.count_neighbor((Y(y * 2), X(x * 2))) == (1, 0) {
+                                ret[(Y(y), X(x))] = Clue(ids[(Y(y), X(x))]);
+                            }
+                        }
+                    }
+
+                    self.pool.push(field);
+                    // release used fields
+                    for used in fields.drain(0..) {
+                        self.pool.push(used);
+                    }
+                    for used in fields_next.drain(0..) {
+                        self.pool.push(used);
+                    }
+
+                    // screening
+                    for mode in 0..2 {
+                        let mut poss = vec![vec![]; id as usize];
+                        for y in 0..height {
+                            for x in 0..width {
+                                poss[ids[(Y(y), X(x))] as usize].push((Y(y), X(x)));
+                            }
+                        }
+
+                        let mut screen_problem = Grid::new(height, width, UNUSED);
+                        let mut used_cells = 0;
+                        for x in 0..width {
+                            let x = if mode == 0 { x } else { width - 1 - x };
+                            for y in 0..height {
+                                let i = ids[(Y(y), X(x))] as usize;
+                                if poss[i].len() > 0 {
+                                    for &loc in &poss[i] {
+                                        if ret[loc] != NO_CLUE {
+                                            screen_problem[loc] = ret[loc];
+                                        } else {
+                                            screen_problem[loc] = NO_CLUE;
+                                        }
+                                        used_cells += 1;
+                                    }
+                                    poss[i].clear();
+                                }
+                            }
+                            if used_cells >= height * width / 2 {
+                                break;
+                            }
+                        }
+
+                        let ans = solve2(&screen_problem, Some(2), false, true);
+                        if ans.len() >= 2 || ans.found_not_fully_filled {
+                            return None;
+                        }
+                    }
+
+                    return Some(ret);
+                }
+
+                fields_next.push(field);
+            }
+
+            // release old fields
+            for old in fields.drain(0..) {
+                self.pool.push(old);
+            }
+
+            ::std::mem::swap(fields, fields_next);
+        }
+        None
     }
 }
 
