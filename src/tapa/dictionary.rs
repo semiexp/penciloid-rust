f2771da03bb4a180524b09afb624fbfde2b1d248
semiexp <linthya.v@gmail.com>
2019-03-02 00:35:52 +0900

    Tapa: add inspect_connectivity_clue_aware

--- src/tapa/dictionary.rs
+++ src/tapa/dictionary.rs
@@ -156,6 +156,160 @@ impl Dictionary {
     }
 }
 
+const CONSECUTIVE_DICTIONARY_NEIGHBOR_PATTERN_COUNT: usize = 256; // 2^8
+const CONSECUTIVE_DICTIONARY_SIZE: usize =
+    CONSECUTIVE_DICTIONARY_NEIGHBOR_PATTERN_COUNT * CLUE_TYPES;
+pub const CONSECUTIVE_DICTIONARY_ADJACENCY_SIZE: usize = 4;
+pub const CONSECUTIVE_DICTIONARY_ADJACENCY_OFFSET: [Coord; CONSECUTIVE_DICTIONARY_ADJACENCY_SIZE] =
+    [(Y(-1), X(0)), (Y(0), X(-1)), (Y(1), X(0)), (Y(0), X(1))];
+const CONSECUTIVE_DICTIONARY_REMOVAL_SIZE: usize = CONSECUTIVE_DICTIONARY_NEIGHBOR_PATTERN_COUNT
+    * DICTIONARY_NEIGHBOR_SIZE
+    * CONSECUTIVE_DICTIONARY_ADJACENCY_SIZE;
+pub struct ConsecutiveRegionDictionary {
+    dic: Vec<u32>,
+    removal_dic: Vec<i32>,
+    chain_size_dic: Vec<i32>,
+}
+impl ConsecutiveRegionDictionary {
+    pub fn new(dic_base: &Dictionary) -> ConsecutiveRegionDictionary {
+        let mut dic = vec![0u32; CONSECUTIVE_DICTIONARY_SIZE];
+        for ty in 0..CLUE_TYPES {
+            for pat_id in 0..CONSECUTIVE_DICTIONARY_NEIGHBOR_PATTERN_COUNT {
+                let mut top = 0;
+                for i in 0..8 {
+                    if ((pat_id >> i) & 1) == 0 {
+                        top = i;
+                        break;
+                    }
+                }
+                let mut chains = vec![];
+                let mut chain_length = 0;
+                for i in 0..8 {
+                    let i = (i + top) % 8;
+                    if ((pat_id >> i) & 1) == 1 {
+                        chain_length += 1;
+                    } else {
+                        if chain_length != 0 {
+                            chains.push(((i + 8 - chain_length) % 8, chain_length));
+                            chain_length = 0;
+                        }
+                    }
+                }
+                if chain_length != 0 {
+                    chains.push(((top + 8 - chain_length) % 8, chain_length));
+                    chain_length = 0;
+                }
+
+                let mut parts = 0u32;
+
+                for i in 0..chains.len() {
+                    let mut pat_id_after_exclusion = pat_id;
+                    let (p, len) = chains[i];
+                    for j in 0..len {
+                        pat_id_after_exclusion ^= 1 << ((p + j) % 8);
+                    }
+
+                    let mut ternary_id = 0;
+                    let mut pow3 = 1;
+                    for j in 0..8 {
+                        if (pat_id_after_exclusion >> j) & 1 == 0 {
+                            ternary_id += pow3 * 2;
+                        }
+                        pow3 *= 3;
+                    }
+
+                    if dic_base.consult_raw(Clue(ty as i32), ternary_id) == DICTIONARY_INCONSISTENT
+                    {
+                        for j in 0..len {
+                            parts |= 1 << (((p + j) % 8) as u32);
+                        }
+                    }
+                }
+
+                dic[ty * CONSECUTIVE_DICTIONARY_NEIGHBOR_PATTERN_COUNT + pat_id] = parts;
+            }
+        }
+
+        let mut prev_direction = [0usize; DICTIONARY_NEIGHBOR_SIZE];
+        let mut next_direction = [0usize; DICTIONARY_NEIGHBOR_SIZE];
+        for i in 0..DICTIONARY_NEIGHBOR_SIZE {
+            let (Y(y), X(x)) = DICTIONARY_NEIGHBOR_OFFSET[i];
+            let (Y(yp), X(xp)) = DICTIONARY_NEIGHBOR_OFFSET[(i + 7) % 8];
+            let (Y(yn), X(xn)) = DICTIONARY_NEIGHBOR_OFFSET[(i + 1) % 8];
+            for j in 0..4 {
+                let (Y(dy), X(dx)) = CONSECUTIVE_DICTIONARY_ADJACENCY_OFFSET[j];
+                if yp == y + dy && xp == x + dx {
+                    prev_direction[i] = j;
+                }
+                if yn == y + dy && xn == x + dx {
+                    next_direction[i] = j;
+                }
+            }
+        }
+
+        let mut removal_dic = vec![0i32; CONSECUTIVE_DICTIONARY_REMOVAL_SIZE];
+        let mut chain_size_dic =
+            vec![0i32; CONSECUTIVE_DICTIONARY_NEIGHBOR_PATTERN_COUNT * DICTIONARY_NEIGHBOR_SIZE];
+        for pat_id in 0..CONSECUTIVE_DICTIONARY_NEIGHBOR_PATTERN_COUNT {
+            for nb in 0..DICTIONARY_NEIGHBOR_SIZE {
+                if (pat_id >> nb) & 1 == 0 {
+                    continue;
+                }
+                let mut prev_count = 0;
+                let mut next_count = 0;
+                if pat_id == CONSECUTIVE_DICTIONARY_NEIGHBOR_PATTERN_COUNT - 1 {
+                    prev_count = 7;
+                } else {
+                    for i in 1..8 {
+                        if (pat_id >> ((nb + 8 - i) % 8)) & 1 != 0 {
+                            prev_count += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    for i in 1..8 {
+                        if (pat_id >> ((nb + i) % 8)) & 1 != 0 {
+                            next_count += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                chain_size_dic[pat_id * DICTIONARY_NEIGHBOR_SIZE + nb] =
+                    prev_count + 1 + next_count;
+
+                if prev_count > 0 && next_count > 0 {
+                    removal_dic[(pat_id * DICTIONARY_NEIGHBOR_SIZE + nb)
+                                    * CONSECUTIVE_DICTIONARY_ADJACENCY_SIZE
+                                    + prev_direction[nb]] = prev_count;
+                    removal_dic[(pat_id * DICTIONARY_NEIGHBOR_SIZE + nb)
+                                    * CONSECUTIVE_DICTIONARY_ADJACENCY_SIZE
+                                    + next_direction[nb]] = next_count;
+                }
+            }
+        }
+
+        ConsecutiveRegionDictionary {
+            dic,
+            removal_dic,
+            chain_size_dic,
+        }
+    }
+    pub fn consult(&self, c: Clue, neighbor_code: u32) -> u32 {
+        let Clue(c) = c;
+        self.dic
+            [c as usize * CONSECUTIVE_DICTIONARY_NEIGHBOR_PATTERN_COUNT + neighbor_code as usize]
+    }
+    pub fn consult_removal(&self, code: u32, neighbor_id: usize, adjacency_id: usize) -> i32 {
+        self.removal_dic[(code as usize * DICTIONARY_NEIGHBOR_SIZE + neighbor_id)
+                             * CONSECUTIVE_DICTIONARY_ADJACENCY_SIZE
+                             + adjacency_id]
+    }
+    pub fn chain_size(&self, code: u32, neighbor_id: usize) -> i32 {
+        self.chain_size_dic[code as usize * DICTIONARY_NEIGHBOR_SIZE + neighbor_id]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -225,6 +379,37 @@ mod tests {
             let mut original = str_to_pattern("?#??#???");
             let clue = clue_pattern_to_id(&[1, 1, 1, 1]).unwrap();
             assert_eq!(complete_dic.consult(clue, &mut original), true);
+        }
+    }
+
+    #[test]
+    fn test_tapa_consecutive_dictionary() {
+        let dic_base = Dictionary::complete();
+        let dic = ConsecutiveRegionDictionary::new(&dic_base);
+
+        {
+            let original = 0b11111111;
+            let clue = clue_pattern_to_id(&[]).unwrap();
+            let expected = 0b00000000;
+            assert_eq!(dic.consult(clue, original), expected);
+        }
+        {
+            let original = 0b11111111;
+            let clue = clue_pattern_to_id(&[1]).unwrap();
+            let expected = 0b11111111;
+            assert_eq!(dic.consult(clue, original), expected);
+        }
+        {
+            let original = 0b11101111;
+            let clue = clue_pattern_to_id(&[3]).unwrap();
+            let expected = 0b11101111;
+            assert_eq!(dic.consult(clue, original), expected);
+        }
+        {
+            let original = 0b11101101;
+            let clue = clue_pattern_to_id(&[1, 2]).unwrap();
+            let expected = 0b11100001;
+            assert_eq!(dic.consult(clue, original), expected);
         }
     }
 }
--- src/tapa/field.rs
+++ src/tapa/field.rs
@@ -1,22 +1,31 @@
 use super::super::{Coord, GraphSeparation, Grid, X, Y};
-use super::{Cell, Clue, Dictionary, CLUE_VALUES, DICTIONARY_INCONSISTENT,
-            DICTIONARY_NEIGHBOR_OFFSET, NO_CLUE};
+use super::{Cell, Clue, ConsecutiveRegionDictionary, Dictionary, CLUE_VALUES,
+            CONSECUTIVE_DICTIONARY_ADJACENCY_OFFSET, CONSECUTIVE_DICTIONARY_ADJACENCY_SIZE,
+            DICTIONARY_INCONSISTENT, DICTIONARY_NEIGHBOR_OFFSET, DICTIONARY_NEIGHBOR_SIZE, NO_CLUE};
 use std::fmt;
+use std::cmp;
 
-pub struct Field<'a> {
+pub struct Field<'a, 'b> {
     cell: Grid<Cell>,
     clue: Grid<Clue>,
     inconsistent: bool,
     dic: &'a Dictionary,
+    consecutive_dic: &'b ConsecutiveRegionDictionary,
 }
 
-impl<'a> Field<'a> {
-    pub fn new(height: i32, width: i32, dic: &'a Dictionary) -> Field {
+impl<'a, 'b> Field<'a, 'b> {
+    pub fn new(
+        height: i32,
+        width: i32,
+        dic: &'a Dictionary,
+        consecutive_dic: &'b ConsecutiveRegionDictionary,
+    ) -> Field<'a, 'b> {
         Field {
             cell: Grid::new(height, width, Cell::Undecided),
             clue: Grid::new(height, width, NO_CLUE),
             inconsistent: false,
             dic,
+            consecutive_dic,
         }
     }
     pub fn height(&self) -> i32 {
@@ -139,6 +148,190 @@ impl<'a> Field<'a> {
             }
         }
     }
+    pub fn inspect_connectivity_clue_aware(&mut self) {
+        let height = self.height();
+        let width = self.width();
+
+        let mut id = Grid::new(height, width, -1);
+        let mut parent_id = Grid::new(height, width, -1);
+        let mut lowlink = Grid::new(height, width, -1);
+        let mut weight = Grid::new(height, width, 0);
+
+        let mut correction_value = Grid::new(height, width, [0, 0, 0, 0]);
+
+        for y in 0..height {
+            for x in 0..width {
+                let cd = (Y(y), X(x));
+                match self.cell(cd) {
+                    Cell::Black => weight[cd] += 1,
+                    Cell::White => if self.clue(cd) != NO_CLUE {
+                        let mut neighbor_pattern = 0u32;
+                        for i in 0..DICTIONARY_NEIGHBOR_SIZE {
+                            let (Y(dy), X(dx)) = DICTIONARY_NEIGHBOR_OFFSET[i];
+                            if self.cell_checked((Y(y + dy), X(x + dx))) != Cell::White {
+                                neighbor_pattern |= 1u32 << i;
+                            }
+                        }
+                        let clue = self.clue(cd);
+                        let affected_neighbors =
+                            self.consecutive_dic.consult(clue, neighbor_pattern);
+
+                        for i in 0..DICTIONARY_NEIGHBOR_SIZE {
+                            let (Y(dy), X(dx)) = DICTIONARY_NEIGHBOR_OFFSET[i];
+                            let cd2 = (Y(y + dy), X(x + dx));
+                            if id.is_valid_coord(cd2) {
+                                weight[cd2] += 1;
+                                for j in 0..CONSECUTIVE_DICTIONARY_ADJACENCY_SIZE {
+                                    correction_value[cd2][j] += self.consecutive_dic
+                                        .consult_removal(affected_neighbors, i, j);
+                                }
+                            }
+                        }
+                    },
+                    Cell::Undecided => (),
+                }
+            }
+        }
+
+        fn visit(
+            cd: Coord,
+            cd_parent: Coord,
+            cell: &Grid<Cell>,
+            id: &mut Grid<i32>,
+            parent_id: &mut Grid<i32>,
+            lowlink: &mut Grid<i32>,
+            weight: &mut Grid<i32>,
+            id_last: &mut i32,
+        ) {
+            if cell[cd] == Cell::White {
+                return;
+            }
+            id[cd] = *id_last;
+            lowlink[cd] = *id_last;
+            *id_last += 1;
+
+            if cd_parent != (Y(-1), X(-1)) {
+                parent_id[cd] = id[cd_parent];
+            }
+
+            let (Y(y), X(x)) = cd;
+
+            for &(Y(dy), X(dx)) in &CONSECUTIVE_DICTIONARY_ADJACENCY_OFFSET {
+                let y2 = y + dy;
+                let x2 = x + dx;
+                if 0 <= y2 && y2 < cell.height() && 0 <= x2 && x2 < cell.width() {
+                    let cd2 = (Y(y2), X(x2));
+                    if cd_parent == cd2 || cell[cd2] == Cell::White {
+                        continue;
+                    }
+                    if id[cd2] == -1 {
+                        // hoge
+                        visit(cd2, cd, cell, id, parent_id, lowlink, weight, id_last);
+                        lowlink[cd] = cmp::min(lowlink[cd], lowlink[cd2]);
+                        weight[cd] += weight[cd2];
+                    } else {
+                        lowlink[cd] = cmp::min(lowlink[cd], id[cd2]);
+                    }
+                }
+            }
+        }
+
+        let mut total_weight = 0;
+
+        'outer: for y in 0..height {
+            for x in 0..width {
+                if self.cell((Y(y), X(x))) != Cell::White {
+                    let mut id_last = 0;
+                    visit(
+                        (Y(y), X(x)),
+                        (Y(-1), X(-1)),
+                        &self.cell,
+                        &mut id,
+                        &mut parent_id,
+                        &mut lowlink,
+                        &mut weight,
+                        &mut id_last,
+                    );
+                    total_weight = weight[(Y(y), X(x))];
+                    break 'outer;
+                }
+            }
+        }
+
+        for y in 0..height {
+            for x in 0..width {
+                if self.cell((Y(y), X(x))) != Cell::White && id[(Y(y), X(x))] == -1 {
+                    self.inconsistent = true;
+                    return;
+                }
+            }
+        }
+
+        for y in 0..height {
+            for x in 0..width {
+                let cd = (Y(y), X(x));
+                let cell = self.cell(cd);
+                if id[cd] == -1 {
+                    continue;
+                }
+                if cell == Cell::Undecided {
+                    let mut local_weights = [0, 0, 0, 0];
+                    let mut parent_dir = 4;
+                    let mut parent_weight = 0;
+                    for i in 0..4 {
+                        let (Y(dy), X(dx)) = CONSECUTIVE_DICTIONARY_ADJACENCY_OFFSET[i];
+                        let cd2 = (Y(y + dy), X(x + dx));
+                        if !id.is_valid_coord(cd2) || id[cd2] == -1 {
+                            continue;
+                        }
+                        if parent_id[cd] == id[cd2] {
+                            // cd2 <- cd
+                            parent_dir = i;
+                            parent_weight += total_weight - weight[cd] - correction_value[cd][i];
+                        } else if parent_id[cd2] == id[cd] {
+                            // cd <- cd2
+                            let w = weight[cd2] - correction_value[cd][i];
+                            if lowlink[cd2] < id[cd] {
+                                parent_weight += w;
+                            } else {
+                                local_weights[i] = w;
+                            }
+                        } else {
+                            // non-DFS edge
+                            if id[cd] < id[cd2] {
+                                let mut closest = (0, 0);
+                                for j in 0..4 {
+                                    let (Y(dy), X(dx)) = CONSECUTIVE_DICTIONARY_ADJACENCY_OFFSET[j];
+                                    let cd3 = (Y(y + dy), X(x + dx));
+                                    if !id.is_valid_coord(cd3) || id[cd3] == -1 {
+                                        continue;
+                                    }
+                                    if id[cd3] <= id[cd2] {
+                                        closest = cmp::max(closest, (id[cd3], j));
+                                    }
+                                }
+                                local_weights[closest.1] -= correction_value[cd][i];
+                            } else {
+                                parent_weight -= correction_value[cd][i];
+                            }
+                        }
+                    }
+                    if parent_dir != 4 {
+                        local_weights[parent_dir] = parent_weight;
+                    }
+                    let mut nonzero = 0;
+                    for i in 0..4 {
+                        if local_weights[i] > 0 {
+                            nonzero += 1;
+                        }
+                    }
+                    if nonzero >= 2 {
+                        self.decide(cd, Cell::Black);
+                    }
+                }
+            }
+        }
+    }
     fn inspect(&mut self, loc: Coord) {
         if !self.cell.is_valid_coord(loc) {
             return;
@@ -179,7 +372,7 @@ impl<'a> Field<'a> {
     }
 }
 
-impl<'a> fmt::Display for Field<'a> {
+impl<'a, 'b> fmt::Display for Field<'a, 'b> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let height = self.height();
         let width = self.width();
@@ -225,8 +418,9 @@ mod tests {
     #[test]
     fn test_tapa_field_clues() {
         let dic = Dictionary::complete();
+        let consecutive_dic = ConsecutiveRegionDictionary::new(&dic);
 
-        let mut field = Field::new(5, 6, &dic);
+        let mut field = Field::new(5, 6, &dic, &consecutive_dic);
         field.add_clue((Y(2), X(1)), clue_pattern_to_id(&[]).unwrap());
         field.add_clue((Y(2), X(3)), clue_pattern_to_id(&[4]).unwrap());
 
@@ -240,8 +434,9 @@ mod tests {
     #[test]
     fn test_tapa_field_cluster() {
         let dic = Dictionary::complete();
+        let consecutive_dic = ConsecutiveRegionDictionary::new(&dic);
 
-        let mut field = Field::new(5, 6, &dic);
+        let mut field = Field::new(5, 6, &dic, &consecutive_dic);
         field.decide(((Y(1), X(1))), Cell::Black);
         field.decide(((Y(1), X(2))), Cell::Black);
         field.decide(((Y(2), X(2))), Cell::Black);
@@ -253,8 +448,9 @@ mod tests {
     #[test]
     fn test_tapa_field_connectivity() {
         let dic = Dictionary::complete();
+        let consecutive_dic = ConsecutiveRegionDictionary::new(&dic);
 
-        let mut field = Field::new(5, 6, &dic);
+        let mut field = Field::new(5, 6, &dic, &consecutive_dic);
         field.decide((Y(0), X(0)), Cell::Black);
         field.decide((Y(4), X(5)), Cell::Black);
         field.decide((Y(1), X(0)), Cell::White);
@@ -273,37 +469,94 @@ mod tests {
     #[test]
     fn test_tapa_field_problem() {
         let dic = Dictionary::complete();
+        let consecutive_dic = ConsecutiveRegionDictionary::new(&dic);
 
-        let mut field = Field::new(6, 5, &dic);
-        field.add_clue((Y(1), X(0)), clue_pattern_to_id(&[1, 3]).unwrap());
-        field.add_clue((Y(1), X(2)), clue_pattern_to_id(&[2, 4]).unwrap());
-        field.add_clue((Y(3), X(1)), clue_pattern_to_id(&[3, 3]).unwrap());
-        field.add_clue((Y(4), X(3)), clue_pattern_to_id(&[4]).unwrap());
+        {
+            let mut field = Field::new(6, 5, &dic, &consecutive_dic);
+            field.add_clue((Y(1), X(0)), clue_pattern_to_id(&[1, 3]).unwrap());
+            field.add_clue((Y(1), X(2)), clue_pattern_to_id(&[2, 4]).unwrap());
+            field.add_clue((Y(3), X(1)), clue_pattern_to_id(&[3, 3]).unwrap());
+            field.add_clue((Y(4), X(3)), clue_pattern_to_id(&[4]).unwrap());
 
-        field.inspect_connectivity();
-        field.inspect_connectivity();
-        field.inspect_connectivity();
+            field.inspect_connectivity();
+            field.inspect_connectivity();
+            field.inspect_connectivity();
 
-        let expected = [
-            [1, 1, 1, 1, 1],
-            [0, 1, 0, 0, 1],
-            [1, 0, 1, 1, 1],
-            [1, 0, 1, 0, 0],
-            [1, 0, 1, 0, 0],
-            [1, 1, 1, 1, 0],
-        ];
-        for y in 0..6 {
-            for x in 0..5 {
-                assert_eq!(
-                    field.cell((Y(y), X(x))),
-                    if expected[y as usize][x as usize] == 1 {
-                        Cell::Black
-                    } else {
-                        Cell::White
-                    }
-                );
+            let expected = [
+                [1, 1, 1, 1, 1],
+                [0, 1, 0, 0, 1],
+                [1, 0, 1, 1, 1],
+                [1, 0, 1, 0, 0],
+                [1, 0, 1, 0, 0],
+                [1, 1, 1, 1, 0],
+            ];
+            for y in 0..6 {
+                for x in 0..5 {
+                    assert_eq!(
+                        field.cell((Y(y), X(x))),
+                        if expected[y as usize][x as usize] == 1 {
+                            Cell::Black
+                        } else {
+                            Cell::White
+                        }
+                    );
+                }
             }
+            assert_eq!(field.inconsistent(), false);
         }
-        assert_eq!(field.inconsistent(), false);
+        {
+            let mut field = Field::new(2, 7, &dic, &consecutive_dic);
+            field.add_clue((Y(0), X(0)), clue_pattern_to_id(&[1]).unwrap());
+            field.add_clue((Y(1), X(2)), clue_pattern_to_id(&[3]).unwrap());
+            field.add_clue((Y(0), X(5)), clue_pattern_to_id(&[2]).unwrap());
+
+            field.inspect_connectivity_clue_aware();
+            field.inspect_connectivity_clue_aware();
+
+            let expected = [[0, 1, 1, 1, 1, 0, 0], [0, 0, 0, 0, 1, 0, 0]];
+            for y in 0..2 {
+                for x in 0..7 {
+                    assert_eq!(
+                        field.cell((Y(y), X(x))),
+                        if expected[y as usize][x as usize] == 1 {
+                            Cell::Black
+                        } else {
+                            Cell::White
+                        }
+                    );
+                }
+            }
+            assert_eq!(field.inconsistent(), false);
+        }
+    }
+
+    #[test]
+    fn test_tapa_field_connectivity_clue_aware() {
+        let dic = Dictionary::complete();
+        let consecutive_dic = ConsecutiveRegionDictionary::new(&dic);
+
+        {
+            let mut field = Field::new(5, 6, &dic, &consecutive_dic);
+            field.add_clue((Y(1), X(3)), clue_pattern_to_id(&[]).unwrap());
+            field.add_clue((Y(4), X(0)), clue_pattern_to_id(&[1]).unwrap());
+            field.add_clue((Y(0), X(0)), clue_pattern_to_id(&[1]).unwrap());
+            field.add_clue((Y(4), X(5)), clue_pattern_to_id(&[1]).unwrap());
+
+            field.inspect_connectivity_clue_aware();
+
+            assert_eq!(field.cell((Y(3), X(1))), Cell::Black);
+            assert_eq!(field.inconsistent(), false);
+        }
+        {
+            let mut field = Field::new(2, 2, &dic, &consecutive_dic);
+            field.add_clue((Y(0), X(0)), clue_pattern_to_id(&[1]).unwrap());
+
+            field.inspect_connectivity_clue_aware();
+
+            assert_eq!(field.cell((Y(0), X(1))), Cell::Undecided);
+            assert_eq!(field.cell((Y(1), X(0))), Cell::Undecided);
+            assert_eq!(field.cell((Y(1), X(1))), Cell::Undecided);
+            assert_eq!(field.inconsistent(), false);
+        }
     }
 }
